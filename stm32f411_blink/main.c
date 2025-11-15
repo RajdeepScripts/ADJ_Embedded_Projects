f59d0986a9968fcbcc70e984b37cb6528bc2d534
Anubhuti <anubhutipattanaik2004@gmail.com>
2025-11-15 16:08:21 +0530

    .ld file is different from original due to gcc 10 differences.

---
+++ stm32f411_blink/main.c
@@ -0,0 +1,28 @@
+#include "stm32f4xx.h"
+
+void delay_ms(uint32_t ms)
+{
+    for (uint32_t i = 0; i < ms * 1000; i++)   // Approximate delay
+    {
+        __NOP();   // Single instruction "No Operation" to waste CPU cycles
+    }
+}
+int main(void)
+{
+    // Enable GPIOA clock (bit 0 in RCC AHB1ENR)
+    RCC->AHB1ENR |= (1 << 0);
+
+    // Set PA5 (pin 5) as output (bit 10 = 1)
+    GPIOA->MODER |= (1 << 10) | (1 << 12);
+
+    while (1)
+    {
+        GPIOA->ODR |= (1 << 5) | (1 << 6);   // LED ON
+       // for (int i = 0; i < 1000000; i++);  // Delay
+       delay_ms(1000);
+        GPIOA->ODR &= ~((1 << 5) | (1 << 6));  // LED OFF
+       // for (int i = 0; i < 1000000; i++);
+       delay_ms(5000);
+    }
+}
+
