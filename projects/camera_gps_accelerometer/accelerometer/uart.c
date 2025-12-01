55d2ff7d1cafd69e946da52fc736d93b0f7227af
Rajdeep <you@example.com>
2025-12-01 11:53:54 +0530

    UART

--- projects/camera_gps_accelerometer/accelerometer/uart.c
+++ projects/camera_gps_accelerometer/accelerometer/uart.c
@@ -1,32 +1,36 @@
+#include "stm32f4xx.h"
 #include "uart.h"
 
-void UART2_Init(void)
-{
-    // Enable clocks
-    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+// Initialize USART2 for UART communication
+void UART2_Init(void) {
+    // Enable GPIOA and USART2 clocks
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
-    // PA2 = TX, PA3 = RX (AF7)
-    GPIOA->MODER &= ~(0xF << 4);
-    GPIOA->MODER |= (0xA << 4); // PA2/PA3 alternate function
+    // Configure PA2 as USART2 TX and PA3 as RX (Alternate function 7)
+    GPIOA->MODER &= ~(GPIO_MODER_MODE2 | GPIO_MODER_MODE3);
+    GPIOA->MODER |= (GPIO_MODER_MODE2_1 | GPIO_MODER_MODE3_1); // AF mode
+    GPIOA->AFR[0] &= ~((0xF << (4 * 2)) | (0xF << (4 * 3)));
+    GPIOA->AFR[0] |= (7 << (4 * 2)) | (7 << (4 * 3));
 
-    GPIOA->AFR[0] |= (7 << 8) | (7 << 12); // AF7 for USART2
-
-    GPIOA->OSPEEDR |= (0xF << 4);
-
-    USART2->BRR = 0x0683; // 115200 baud
-    USART2->CR1 = USART_CR1_TE | USART_CR1_UE;
+    // Configure USART2 parameters: 9600 baud, 8N1
+    USART2->BRR = 16000000 / 115200;  // Assuming 16 MHz APB1 clock
+    USART2->CR1 = USART_CR1_TE | USART_CR1_RE;  // Enable TX, RX
+    USART2->CR2 = 0;  // 1 stop bit
+    USART2->CR3 = 0;
+    USART2->CR1 |= USART_CR1_UE;  // Enable USART2
 }
 
-void UART2_SendChar(char c)
-{
+// Send a character over USART2
+void UART2_SendChar(char c) {
     while (!(USART2->SR & USART_SR_TXE));
     USART2->DR = c;
 }
 
-void UART2_SendString(const char *str)
-{
-    while (*str)
+// Send a null-terminated string over USART2
+void UART2_SendString(const char *str) {
+    while (*str) {
         UART2_SendChar(*str++);
+    }
 }
 
