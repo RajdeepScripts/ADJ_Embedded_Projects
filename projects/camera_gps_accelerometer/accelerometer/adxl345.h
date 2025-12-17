b484bc3e2a572fe3787bbaba5be97cbe65299802
Rajdeep <you@example.com>
2025-12-17 15:19:17 +0530

    Clean Branch

--- projects/camera_gps_accelerometer/accelerometer/adxl345.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADXL345_H
-#define ADXL345_H
-
-#include <stdint.h>
-
-void ADXL345_Init(void);
-void ADXL345_ReadXYZ(int16_t *x, int16_t *y, int16_t *z);
-
-#endif
--- projects/camera_gps_accelerometer/accelerometer/adxl345reg.c
+++
@@ -1,8 +0,0 @@
-#define ADXL345_ADDR       0x53
-
-#define REG_DEVID          0x00
-#define REG_POWER_CTL      0x2D
-#define REG_DATA_FORMAT    0x31
-#define REG_BW_RATE        0x2C
-#define REG_DATAX0         0x32
-
